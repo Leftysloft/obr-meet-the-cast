88257157047324e78fe3e1b8820403652f6b0a21
Leftysloft <patrickbglose@gmail.com>
2025-05-24 12:32:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import "../style.css";
+import "./style.css";
 import OBR from "@owlbear-rodeo/sdk";
 import { setupContextMenu } from "./contextMenu.js";
 import { setupSettings } from "./settings/settings.js";
