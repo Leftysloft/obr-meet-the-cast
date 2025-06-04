66baa8ef3b9e40e870e6f48ffcaa8bccf1e31671
Leftysloft <patrickbglose@gmail.com>
2025-06-04 01:33:46 -0400

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -56,6 +56,8 @@ OBR.onReady(async () => {
     handleSceneItems(items);
 
     OBR.scene.items.onChange((items) => {
+        const allItems = await OBR.scene.items.getItems();
+
       handleSceneItems(items);
     });
 
