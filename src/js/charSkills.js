9e6d33faf910ca0762b06baf24b20062cfe4e23d
Leftysloft <patrickbglose@gmail.com>
2025-06-04 13:23:59 -0400

    Update charSkills.js

--- src/js/charSkills.js
+++ src/js/charSkills.js
@@ -112,9 +112,9 @@ export async function renderSkills(charId) {
           const result = rollStat(`${prettyNames[athletics]} Check`, modifier);
           showRollPopover(result.label, result.display, characterName);
           OBR.broadcast?.sendMessage?.("rodeo.owlbear.charSkills.rollResult", {
-            label: result.label,
-            content: result.display,
-            name: characterName,
+            label: String(result.label),
+            content: String(result.display),
+            name: String(characterName),
           });
         });
 
