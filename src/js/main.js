348614303116d35588e12e0e636c86e7433ce1f0
Leftysloft <patrickbglose@gmail.com>
2025-06-04 13:12:58 -0400

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -114,7 +114,7 @@ OBR.onReady(async () => {
 
       if (
         type === "rodeo.owlbear.charStats.rollResult" ||
-        type === "rodeo.owlbear.charskills.rollResult"
+        type === "rodeo.owlbear.charSkills.rollResult"
       ) {
         showRollPopover(label, content, name);
       }
