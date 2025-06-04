8dc1d16f4e664e3df6fe186c5364184e71d3412d
Leftysloft <patrickbglose@gmail.com>
2025-06-04 13:03:44 -0400

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -107,21 +107,17 @@ OBR.onReady(async () => {
     }
   });
 
-  // Broadcast stats to players
   if (OBR.broadcast?.onMessage) {
-    OBR.broadcast.onMessage("rodeo.owlbear.charStats.rollResult", (event) => {
+    OBR.broadcast.onMessage((event) => {
       const { label, content, name } = event.data;
-      showRollPopover(label, content, name);
-    });
-  } else {
-    console.warn("Broadcast listener unavailable");
-  }
+      const type = event.type;
 
-  // Broadcast skills to players
-  if (OBR.broadcast?.onMessage) {
-    OBR.broadcast.onMessage("rodeo.owlbear.charskills.rollResult", (event) => {
-      const { label, content, name } = event.data;
-      showRollPopover(label, content, name);
+      if (
+        type === "rodeo.owlbear.charStats.rollResult" ||
+        type === "rodeo.owlbear.charskills.rollResult"
+      ) {
+        showRollPopover(label, content, name);
+      }
     });
   } else {
     console.warn("Broadcast listener unavailable");
